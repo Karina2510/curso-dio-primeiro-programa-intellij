c06dd38c0ad25fe285f2a1e88d3ff87b123e7b1f
Karina2510 <karina.ferreira.s1995@gmail.com>
2022-04-24 21:08:22 -0300

    "criando class livro"

--- primeiro-programa-intellij/src/br/com/dio/PrimeiroPrograma.java
+++ primeiro-programa-intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -7,6 +7,7 @@ public class PrimeiroPrograma {
     public static void main(String[] args) {
 
         Gato gato = new Gato();
+        Livro livro1 = new Livro(nome: "O problema dos copos", numPaginas 300);
         System.out.println(gato);
 
 //        int a = 5;
@@ -15,3 +16,40 @@ public class PrimeiroPrograma {
     }
 
 }
+    class Livro{
+        private String nome;
+        private Integer numPaginas;
+
+        @java.lang.Override
+        public java.lang.String toString() {
+            return "Livro{" +
+                    "nome='" + nome + '\'' +
+                    ", numPaginas=" + numPaginas +
+                    '}';
+        }
+
+        public Livro(String nome, Integer numPaginas) {
+            this.nome = nome;
+            this.numPaginas = numPaginas;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public Integer getNumPaginas() {
+            return numPaginas;
+        }
+
+        public void setNumPaginas(Integer numPaginas) {
+            this.numPaginas = numPaginas;
+        }
+    }
+
+
+
+
